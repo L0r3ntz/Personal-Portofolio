7b45a039c6a3be2d24232ec5ce0ef6e10a28f00d
L0r3ntz <scell3228@gmail.com>
2024-06-08 13:01:53 +0700

    second commit

---
+++ src/components/contents/ExperienceComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="slide-right w-full rounded-md bg-background sm:p-5 max-sm:p-3 shadow-custom">
+        <h1 class="sm:text-xl max-sm:text-lg font-semibold mb-3"><span class="text-title">Experience</span></h1>
+        <ul class="flex flex-col space-y-1 list-disc pl-5 sm:pr-3 max-sm:text-sm">
+            <li>Editorial Desainer Grafis</li>
+            <p class="text-sm">Pernah Menjadi Editorial Desainer Grafis Di Adiva Indonesia pada bulan September 2023 - Mei 2024</p>
+            <li>Ketua Organinasi</li>
+            <p class="text-sm">menjabat sebagai ketua di organisasi Budidarma Desain Grafis & Multimedia Club tahun 2023 sampai sekarang.</p>
+            <li>Instruktur Desain Grafis</li>
+            <p class="text-sm">Menjadi mentor atau instruktur pada organisasi Budidarma Desain Grafis & Multimedia Club.</p>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'blog-component',
+}
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue, js, ts, jsx, tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      'background': '#29241e',
+      'text': '#f7f9fa',
+      'title': '#4cb6f7',
+      'title-hover': '#5ca8ff',
+      'bg': '#1d2120',
+    },
   },
   plugins: [],
 }
